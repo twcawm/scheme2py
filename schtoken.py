2837b2664ab4b5b1606de792938813448c34ba8c
twcawm <twcawm@gmail.com>
2022-03-05 17:05:16 -0500

    update tokenizer for identifier recognition

--- schtoken.py
+++ schtoken.py
@@ -7,10 +7,10 @@ class Token:
 
 class Tokenizer:
   #l_symbols = [r"(", r")", r"+", r"-", r"*", r"/", r'<', r'>', r'='] #have to update this later
-  l_symbols = [r"(", r")", r"/"] #have to update this later
+  l_symbols = [r"(", r")"] #we're basically only using this category for parentheses
   re_symbol = r'[' +re.escape(r"".join(l_symbols)) + r']'
   re_number = r"\d*\.\d+|\d+(?!\.)"
-  re_identifier = r"[\w\-+*<>=/]+" #+ means at least 1.  \w means word character.
+  re_identifier = r"[\w\-+*<>=/?!]+" #+ means at least 1.  \w means word character.
     #identifier may have to be updated since scheme allows more identifiers than typical.
     #only - has to be escaped inside of a regex set []
   re_string_const = r'"[^"\n]*"' #starts with ".  [^...] denotes COMPLEMENT of a group. so match any number of anything except newlines and other ", then end with ".
