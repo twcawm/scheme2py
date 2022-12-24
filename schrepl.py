49a41b2613f34de6aaff6d4a150952f028c6d422
twcawm <twcawm@gmail.com>
2022-12-24 16:34:38 -0500

    implementing repl commands such as exit and run-file

--- schrepl.py
+++ schrepl.py
@@ -3,14 +3,40 @@ import schtoken
 import schparser
 import scheval
 
+repl_commands = {}
+#some commands will be built in to the repl rather than the eval-apply loop
+def run_file(filename):
+  with open(filename) as f:
+    f_lines = f.readlines()
+    for fl in f_lines:
+      print(fl)
+  '''
+  tokenizer.set_input(str_in)
+  parser.set_input(tokenizer.l_tokens)
+  abstract_syntax_tree = parser.get_syntax_tree()
+  str_out = scheval.evals(abstract_syntax_tree)
+  print(str_out)
+  '''
+repl_commands[":run"] = run_file
+for cmd in [":quit",":q",":e",":exit"]:
+  repl_commands[cmd] = exit
+
 #initialize tokenizer and parser
 tokenizer = schtoken.Tokenizer()
 parser = schparser.Parser()
 
 while(True):
   str_in = input("scheme2py:=> ")
+  preparse = str_in.split()
+  if preparse[0] in repl_commands:
+    print("going to call " + str(repl_commands[preparse[0]]) + " with arguments " + str(preparse[1:]))
+    continue
+    #repl_commands[preparse[0]](preparse[1:])
+  else:
+    pass
   tokenizer.set_input(str_in)
   parser.set_input(tokenizer.l_tokens)
   abstract_syntax_tree = parser.get_syntax_tree()
   str_out = scheval.evals(abstract_syntax_tree)
   print(str_out)
+
