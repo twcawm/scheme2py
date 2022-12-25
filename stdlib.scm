2d27d2c57a3fe35e68e7a2049f371a5ea872c8e9
twcawm <twcawm@gmail.com>
2022-12-25 01:52:30 -0500

    implement delay

--- stdlib.scm
+++ stdlib.scm
@@ -1,3 +1,4 @@
 (define cons (lambda (x y) (lambda (m) (m x y ))))
 (define car (lambda (z) (z (lambda (p q) p ))) )
 (define cdr (lambda (z) (z (lambda (p q) q ))) )
+(define delay (lambda (expr) (lambda () expr)))
