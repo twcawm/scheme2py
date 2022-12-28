364e0bf49b4d2c9a1504d6fffa6169831b05782b
twcawm <twcawm@gmail.com>
2022-12-28 16:27:52 -0500

    reformat comments in testtokenizer

--- testtokenizer.py
+++ testtokenizer.py
@@ -5,18 +5,18 @@ import scheval
 tok = schtoken.Tokenizer()
 psr = schparser.Parser()
 
-#tok.set_input("(+ 1 2)") #works
-#tok.set_input("(begin (define twice (lambda (x) (* 2 x))) (define repeat (lambda (f) (lambda (x) (f (f x))))) ( (repeat twice) 10))") #works
-#tok.set_input("(define area- (- 3 4))")
-#tok.set_input("(begin (define circle-area (lambda (r) (* pi (* r r)))) (circle-area 3))" )
-#tok.set_input("true") #boolean works
-#tok.set_input("#f") # hashed boolean val now works
+# tok.set_input("(+ 1 2)") #works
+# tok.set_input("(begin (define twice (lambda (x) (* 2 x))) (define repeat (lambda (f) (lambda (x) (f (f x))))) ( (repeat twice) 10))") #works
+# tok.set_input("(define area- (- 3 4))")
+# tok.set_input("(begin (define circle-area (lambda (r) (* pi (* r r)))) (circle-area 3))" )
+# tok.set_input("true") #boolean works
+# tok.set_input("#f") # hashed boolean val now works
 tok.set_input("(begin (define a 1) (define b a) (eq? a b))")
-#tok.set_input("(begin (define twice (lambda (x) (* 2 x))) (twice 5))") #works
-#tok.set_input("(begin (define fds (lambda (x) (+ x 1))) (fds 3))") #works
-#tok.set_input("((lambda (x) (+ x 1)) 2)") #fixed! works
-#tok.set_input(("(begin (define  a 1.1 ) (define b (+ 2 3)) (* a b)) (begin (+ 0 (+ (- 2 3 4 ) 5))); hello there \n #|(+ 2 3)|#\n (+ 4 5) (+ .6 7.8)")) #works
-#tok.set_input("(< 4 5)") #works
+# tok.set_input("(begin (define twice (lambda (x) (* 2 x))) (twice 5))") #works
+# tok.set_input("(begin (define fds (lambda (x) (+ x 1))) (fds 3))") #works
+# tok.set_input("((lambda (x) (+ x 1)) 2)") #fixed! works
+# tok.set_input(("(begin (define  a 1.1 ) (define b (+ 2 3)) (* a b)) (begin (+ 0 (+ (- 2 3 4 ) 5))); hello there \n #|(+ 2 3)|#\n (+ 4 5) (+ .6 7.8)")) #works
+# tok.set_input("(< 4 5)") #works
 print("input is: ")
 print(tok.input)
 print("lex element list:")
@@ -29,6 +29,6 @@ psr.set_input(tok.l_tokens)
 ast = psr.get_syntax_tree()
 print("syntax tree:")
 print(psr.lex_tree)
-#psr.print_token_tree()
+# psr.print_token_tree()
 
 print("value: " + str(scheval.evals(ast)))
